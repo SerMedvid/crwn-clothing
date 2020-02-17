4c20eb2d485fad504fe9fcc715a4995ba916bbb4
Serhii Medvid <mr.medvid@gmail.com>
2020-02-17 21:38:05 +0200

    finishing sign in handling method

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ class App extends React.Component {
               ...snapShot.data()
             }
           })
-
-          console.log({currentUser: userAuth});
         });
       } else {
         this.setState({currentUser: userAuth});
