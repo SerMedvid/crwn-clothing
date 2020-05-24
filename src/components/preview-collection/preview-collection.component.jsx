4ac6dd1aab82ec437bd4b414f2e1285831c3ded2
Serhii Medvid <mr.medvid@gmail.com>
2020-05-24 13:07:04 +0300

    updated shop data, added addToCart action

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -9,8 +9,8 @@ const PreviewCollection = ({title, items}) => (
     <div className="preview">
       {
         items
-          .filter((item, idx) => idx < 4).map(({id, ...otherItemsProps}) => (
-            <CollectionItem key={id} {...otherItemsProps} />
+          .filter((item, idx) => idx < 4).map((item) => (
+            <CollectionItem key={item.id} item={item} />
         ))
       }
     </div>
