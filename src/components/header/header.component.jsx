f9c9775e30377aa415b375052f064992a075d5e4
Serhii Medvid <mr.medvid@gmail.com>
2020-05-24 21:35:49 +0300

    refactored with reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
 import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import CartIcon from "../cart-icon/cart-icon-component";
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {setCurrentUser} from "../../redux/user/user.actions";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import './header.styles.scss';
 
@@ -29,9 +32,9 @@ const Header = ({currentUser, hidden}) => (
   </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: setCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,3 +12,8 @@ export const selectCartItemsCount = createSelector(
     cartItems =>
         cartItems.reduce((accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity, 0)
 );
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    (cart) => cart.hidden
+);
