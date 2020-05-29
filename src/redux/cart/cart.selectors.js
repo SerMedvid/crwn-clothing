dd7aa7185184be944b3f9e7b6e77104f84b69d1e
Serhii Medvid <mr.medvid@gmail.com>
2020-05-29 20:32:31 +0300

    added checkout component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,12 @@ export const selectCartHidden = createSelector(
     [selectCart],
     (cart) => cart.hidden
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce((accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity * cartItems.price,
+            0
+        )
+    )
+;
