43a48f354febac00451102d58640a4f28cd822bd
Serhii Medvid <mr.medvid@gmail.com>
2020-05-30 20:54:58 +0300

    updated redux flow

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './collections-overview.styles.scss';
+import  {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
+import CollectionPreview from "../preview-collection/preview-collection.component";
+import {selectCollectionForPreview} from "../../redux/shop/shop.selectors";
+
+const CollectionsOverview = ({collections}) => (
+    <div className='collection-overview'>
+        {collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionForPreview
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import CollectionPreview from '../../components/preview-collection/preview-collection.component';
-import {createStructuredSelector} from "reselect";
-import {connect} from 'react-redux';
-import {selectCollections} from "../../redux/shop/shop.selectors";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import {Route} from 'react-router-dom';
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
     <div className="shop-page">
-      {collections.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+        <Route component={CollectionsOverview} exact path={`${match.path}`}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
