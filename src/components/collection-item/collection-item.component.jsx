410330844bbb9f025d4a5c73c106f1e184bb8a57
Serhii Medvid <mr.medvid@gmail.com>
2020-07-05 11:28:03 +0300

    moved shop data to backend

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import {connect} from 'react-redux'
-
-import CustomButton from "../custom-button/custom-button.component";
 import {addItem} from "../../redux/cart/cart.actions";
-
-import './collection-item.styles.scss';
+import {
+    CollectionItemContainer,
+    AddButton,
+    BackgroundImage,
+    CollectionFooterContainer,
+    NameContainer,
+    PriceContainer} from "./collection-item.styles";
 
 const CollectionItem = ({item, addItem}) => {
     const {name, price, imageUrl} = item;
     return(
-      <div className='collection-item'>
-        <div
-          className='image'
-          style={{backgroundImage: `url(${imageUrl})`}}
+      <CollectionItemContainer>
+        <BackgroundImage
+            className='image'
+            style={{backgroundImage: `url(${imageUrl})`}}
         />
-        <div className='collection-footer'>
-          <span className='span'>{name}</span>
-          <span className='price'>{price}</span>
-        </div>
-          <CustomButton onClick={() => addItem(item)} inverted>Add to Cart</CustomButton>
-      </div>
+        <CollectionFooterContainer>
+          <NameContainer>{name}</NameContainer>
+          <PriceContainer>{price}</PriceContainer>
+        </CollectionFooterContainer>
+          <AddButton onClick={() => addItem(item)} inverted>Add to Cart</AddButton>
+      </CollectionItemContainer>
 )};
 
 const mapDispatchToProps = dispatch => ({
